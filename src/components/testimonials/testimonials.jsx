1ff0b098373cb57e3de722dfdc3d21d1c9701d22
Korede <oyeyemikorede5@gmail.com>
2024-06-30 13:29:37 +0100

    Krypto

---
+++ src/components/testimonials/testimonials.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <>
+      <div className="testimonialHead">
+        <h4>TESTIMONIES</h4>
+        <h1>Read What Others Have To Say</h1>
+      </div>
+    </>
+  );
+};
+
+export default Testimonials;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+export default {
+  build: {
+    rollupOptions: {
+      external: ["react-router-dom"],
+    },
+  },
+};
